bdb211feb9db23a3a26d09f8f65f1189a5fbc9fc
Andrew Erickson <aerickson@gmail.com>
2020-11-02 01:50:24 -0800

    remove duplicated test, organize imports

--- pyfiglet/tests/test_cli.py
+++ pyfiglet/tests/test_cli.py
@@ -1,37 +1,13 @@
 import os
-import subprocess32
+
 import pytest
+import subprocess32
 
 
 @pytest.fixture
 def test_font_dir():
     swd = os.path.dirname(os.path.abspath(__file__))
     return os.path.abspath(swd)
-
-
-def test_strip():
-    command = "pyfiglet -f doh -s 0"
-    expected = '''\
-     000000000     
-   00:::::::::00   
- 00:::::::::::::00 
-0:::::::000:::::::0
-0::::::0   0::::::0
-0:::::0     0:::::0
-0:::::0     0:::::0
-0:::::0 000 0:::::0
-0:::::0 000 0:::::0
-0:::::0     0:::::0
-0:::::0     0:::::0
-0::::::0   0::::::0
-0:::::::000:::::::0
- 00:::::::::::::00 
-   00:::::::::00   
-     000000000
-'''
-    result = subprocess32.run(command, shell=True, stdout=subprocess32.PIPE)
-    assert result.stdout.decode() == expected
-    assert result.returncode == 0
 
 
 def test_strip():
